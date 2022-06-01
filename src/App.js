6409cebbf6098f155ccc0b7e9d2cd7031c11724f
AllenAlexander98 <73653888+AllenAlexander98@users.noreply.github.com>
2022-06-01 20:47:58 +0700

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 // eslint-disable-next-line
 import { Route } from "react-router-dom";
-
-const HatsPage = () => <h1>HATS PAGE</h1>;
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
